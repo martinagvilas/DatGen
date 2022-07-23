1a3c9c4abe3bdd053e0e31b5ae8a50e8c8a4f1cd
xiaxu0613 <xuxiaxuzhonghua@gmail.com>
2022-07-23 21:14:03 +0200

    Update run.py

--- datgen/app/run.py
+++ datgen/app/run.py
@@ -4,7 +4,7 @@ import argparse
 # initial specification formula
 init_spec = {
     'Object name': '',
-    'Object size': [50, 50],
+    'Object size': 50,
     'Visual attributes': [''],
     'Location': [''],
     'Match contrast': 'No',
@@ -68,21 +68,13 @@ if __name__ == '__main__':
         form = st.form(key=f'{i}_obj_form', clear_on_submit=True)
         with form:
             # Define object
-            obj_name = st.text_input('Name of object', key=f'{i}_obj_name',
-                                     value=current_spec['Object name'])
+            name = st.text_input('Name of object', key=f'{i}_obj_name',
+                                 value=current_spec['Object name'])
 
             # Define size
-            st.markdown('__Size__')
-            cols_s = st.columns(3)
-            size_random = cols_s[0].checkbox("Random", key=f'{i}_rand_size',
-                                             value=True if current_spec['Object size'] == 'Random' else False)
-            size_min = cols_s[1].number_input(
-                'Min occupancy (%)', min_value=0, max_value=100, step=5, key=f'{i}_min_size',
-                value=current_spec['Object size'][0] if current_spec['Object size'] != 'Random' else 50)
-            size_max = cols_s[2].number_input(
-                'Max occupancy (%)', min_value=0, max_value=100, step=5, key=f'{i}_max_size',
-                value=current_spec['Object size'][1] if current_spec['Object size'] != 'Random' else 50)
-
+            size = st.number_input('Minimum occupancy of object (%)', help='(...). Example:',
+                                   min_value=0, max_value=100, step=5, key=f'{i}_min_size',
+                                   value=current_spec['Object size'])
             # Define visual attributes
             st.markdown('__Visual Attributes__')
             vis_att = st.text_input(
@@ -91,14 +83,8 @@ if __name__ == '__main__':
                     current_spec['Visual attributes']))
 
             # Define location
-            st.markdown('__Location__')
-            cols_l = st.columns(2)
-            loc_random = cols_l[0].checkbox("Random", key=f'{i}_rand_loc',
-                                            value=True if current_spec['Location'] == 'Random' else False)
-            loc = cols_l[1].text_input(
-                label='List of attributes for location separated by ";"', key=f'{i}_loc',
-                value='' if current_spec['Location'] == 'Random' else ';'.join(current_spec['Location']))
-
+            loc = st.text_input('Location/s of the object (separated by ";")', key=f'{i}_loc', help='(...). Example:',
+                                value=current_spec['Location'])
             # Define global visual attributes
             st.markdown('__Global attributes__')
             cols_ga = st.columns(2)
@@ -109,25 +95,12 @@ if __name__ == '__main__':
 
             submitted = st.form_submit_button(label="Submit")
             if submitted:
-                spec = {'Object name': obj_name}
-
-                if size_random:
-                    spec['Object size'] = 'Random'
-                else:
-                    spec['Object size'] = [size_min, size_max]
-
-                spec['Visual attributes'] = vis_att.split(';')
-
-                if loc_random:
-                    spec['Location'] = 'Random'
-                else:
-                    spec['Location'] = loc.split(';')
-
-                spec['Match contrast'] = contr
-                spec['Match luminance'] = lum
-
+                spec = {'Object name': name,
+                        'Object size': size,
+                        'Visual attributes': vis_att.split(';'),
+                        'Location': loc.split(';'),
+                        'Match contrast': contr, 'Match luminance': lum}
                 st.session_state['specs'][i] = spec
                 st.experimental_rerun()
-    # print the specs for debugging
-    st.write(st.session_state['specs'])
-
+# print the specs for debugging
+st.write(st.session_state['specs'])
