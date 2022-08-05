c6e66b098e38aa89bff34d88fe4e80c14f37c3d5
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-08-05 22:23:30 +0200

    add code for caption match

--- datgen/image_match/caption_match.py
+++ datgen/image_match/caption_match.py
@@ -1,1 +1,30 @@
-# Priority three: doesn't match anything --> if match not enough compute remaining igms
+import clip
+
+from datgen.image_match.annot_search import search_annotations
+
+
+## how to threshold??
+
+
+def compute_match(inputs):
+
+    device='cpu'
+    model, _ = clip.load('ViT-B/32', device)
+    model.to(device)
+
+    imgs_ids = search_annotations(inputs)
+    
+    for dataset in imgs_ids.keys():
+        p1_imgs = imgs_ids[dataset]
+        for i in p1_imgs:
+            # load image tensor
+            # get input 
+            # compute match
+            pass
+    # call search annotations
+    return
+
+
+# def clip_match(imgs_ids, ):
+
+#     return
