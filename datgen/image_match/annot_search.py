4104f343debc51b289331a11141bfa7a3e78693a
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-08-05 21:56:07 +0200

    add visual genome search

--- datgen/image_match/annot_search.py
+++ datgen/image_match/annot_search.py
@@ -1,9 +1,15 @@
+import json
 from pathlib import Path
 
 import pandas as pd
 
 
 ANNOT_PATH = Path('/Users/m_vilas/uni/software_engineering/DatGen/datasets/')
+
+## Ideas for pre-optimization
+# VG: list of objects and only look if object is present
+## --> same for CC but using labels and words of captions
+
 
 
 def search_annotations(inputs):
@@ -29,9 +35,12 @@ def search_annotations(inputs):
     # Search Conceptual Captions dataset
     imgs_ids['cc'] = search_cc(inputs)
 
-    # TODO: Search Visual Genome dataset
+    # Search Visual Genome dataset
+    imgs_ids['vg'] = search_vg(inputs)
 
     # TODO: Search Open Images dataset
+
+    ## TODO what happens if empty --> deal with it in caption match?
 
     return imgs_ids
 
@@ -69,7 +78,10 @@ def search_cc(inputs):
 
 
 def get_cc_object_info(obj, labels):
-    captions_file = ANNOT_PATH / f'cc_training_captions.csv'
+
+## TODO: search testing captions too
+
+    captions_file = ANNOT_PATH / 'cc' / f'cc_training_captions.csv'
     captions = pd.read_csv(captions_file, sep=',')
 
     # Search by tag
@@ -82,3 +94,74 @@ def get_cc_object_info(obj, labels):
     object_info = captions.loc[captions['file'].isin(imgs_ids)]
 
     return object_info
+
+
+def search_vg(inputs):
+    # Load object dictionary
+    try:
+        with open((ANNOT_PATH / 'vg' / 'object_info.json'), 'r') as f:
+            obj_info = json.load(f)
+    except:
+        obj_info = create_vg_obj_dict()
+    
+    # Search if requested objects are in dataset and if not return empty list
+    obj_present = False
+    for obj_vals in inputs.values():
+        if obj_vals['obj'] in obj_info.keys():
+            obj_present = True
+            break
+    if obj_present == False:
+        return []
+    
+    # Search for attributes
+    attr_file = ANNOT_PATH / 'vg' / 'attributes.json'
+    with open(attr_file, 'r') as f:
+        attr_info = json.load(f)
+    imgs = {}
+    for obj, vals in inputs.items():
+        obj_name = vals['obj']
+        obj_attr = vals['vis_attr']
+        imgs_obj = obj_info[obj_name]
+        imgs_attr = []
+        for img in attr_info:
+            img_id = img['image_id']
+            if img_id in imgs_obj:
+                for i in img['attributes']:
+                    try:
+                        if (obj_name in i['names']) & \
+                            (any(a in i['attributes'] for a in obj_attr)):
+                            imgs_attr.append(img_id)
+                    except:
+                        continue
+        imgs_attr = list(set(imgs_attr))
+        imgs_loc = [obj_info[l] for l in vals['loc']]
+        imgs_loc = [i for l in imgs_loc for i in l]
+
+        imgs[obj] = {}
+        imgs[obj]['p1'] = [i for i in imgs_attr if i in imgs_loc]
+        match_imgs = imgs_attr + imgs_loc
+        imgs[obj]['p2'] = [i for i in match_imgs if i not in imgs[obj]['p1']]
+        imgs[obj]['p3'] = [i for i in imgs_obj if i not in match_imgs]
+    
+    return imgs
+
+
+def create_vg_obj_dict():
+    attr_file = ANNOT_PATH / 'vg' / f'attributes.json'
+    with open(attr_file, 'r') as f:
+        attr_info = json.load(f)
+    obj_dict = {}
+    for img_info in attr_info:
+        img_id = img_info['image_id']
+        for obj in img_info['attributes']:
+            obj_name = obj['names'][0]
+            try:
+                #if img_id not in obj_dict[obj_name]:
+                obj_dict[obj_name] += [img_id]
+            except KeyError:
+                obj_dict[obj_name] = [img_id]
+    for obj_name, obj_vals in obj_dict.items():
+        obj_dict[obj_name] = list(set(obj_vals))
+    with open((ANNOT_PATH / 'vg' / 'object_info.json'), 'w') as f:
+        json.dump(obj_dict, f)
+    return obj_dict
