b04e4ea5349ec19f7075bb1c51aaf55d00d27f79
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-08-27 14:44:47 +0200

    move annotations dictory to config file

---
+++ datgen/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+## PATH
+current_path = Path().parent.resolve()
+if 'm_vilas' in str(current_path):
+    ANNOT_PATH = Path(
+        '/Users/m_vilas/uni/software_engineering/DatGen/datasets/annot'
+    )
+    IMGS_PATH = Path(
+        '/Users/m_vilas/uni/software_engineering/DatGen/datasets/images'
+    )
+else:
+    ANNOT_PATH = Path('../../data/datgen_data/image_metas/annot')
+    IMGS_PATH = Path('../../data/datgen_data/image_metas/images')
